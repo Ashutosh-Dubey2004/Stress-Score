f67a3950661612b54b2a7b3bc5ad87c6a994eee1
Ashutosh Dubey <ashutoshmddubey@gmail.com>
2023-06-13 09:52:24 +0530

    Change File Location

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 DEBUG_MODE = False
 DBNAME      = "entries.db"
-DBLOC       = f"{os.environ['USERPROFILE']}\\.survey-data\\"
+DBLOC       = f"{os.environ['USERPROFILE']}\\.survey-sheets\\"
 DB          = DBLOC + DBNAME
-REPORTSFOL  = f"{os.environ['USERPROFILE']}\\.survey-data\\reports"
-RAWFOL      = f"{os.environ['USERPROFILE']}\\.survey-data\\raw"
-TEMPDIR     = f"{os.environ['USERPROFILE']}\\.survey-data\\TEMP"
-LOGDIR      = f"{os.environ['USERPROFILE']}\\.survey-data\\logs"
+REPORTSFOL  = f"{os.environ['USERPROFILE']}\\.survey-sheets\\reports"
+RAWFOL      = f"{os.environ['USERPROFILE']}\\.survey-sheets\\raw"
+TEMPDIR     = f"{os.environ['USERPROFILE']}\\.survey-sheets\\TEMP"
+LOGDIR      = f"{os.environ['USERPROFILE']}\\.survey-sheets\\logs"
 LOGFILE     = f"{LOGDIR}\\simple-log.txt"
